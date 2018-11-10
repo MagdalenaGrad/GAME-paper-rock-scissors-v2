1bae5db62e0686a7c27ef2f7898f8e681c75146e
Magdalena Grad <mgdln.grad@gmail.com>
2018-11-10 18:18:13 +0100

    add functions game.js

--- game.js
+++ game.js
@@ -1,5 +1,3 @@
-// Zobacz gotowy projekt: https://websamuraj.pl/examples/js/projekt7/
-
 const gameSummary = {
     numbers: 0,
     wins: 0,
@@ -35,17 +33,44 @@ function checkResult(player, ai) {
     if (player == ai) return "draw"
     else if ((player === "paper" && ai === "rock") || (player === "rock" && ai === "scissors") || (player === "scissors" && ai === "paper")) return "win"
     else if ((player === "paper" && ai === "scissors") || (player === "rock" && ai === "paper") || (player === "scissors" && ai === "rock")) return "loss"
-    else return "draw"
-
+    else return "draw";
 };
+//result
+function publishResult(player, ai, result) {
+    document.querySelector('[data-summary="your-choice"]').textContent = player;
+    document.querySelector('[data-summary="ai-choice"]').textContent = ai;
+    document.querySelector('p.numbers span').textContent = ++gameSummary.numbers;
 
+    if (result === "win") {
+        document.querySelector('p.wins span').textContent = ++gameSummary.wins;
+        document.querySelector('[data-summary="who-win"]').textContent = "You won!!!";
+        document.querySelector('[data-summary="who-win"]').style.color = "green";
+    } else if (result === "loss") {
+        document.querySelector('p.losses span').textContent = ++gameSummary.losses;
+        document.querySelector('[data-summary="who-win"]').textContent = "Computer won!!!";
+        document.querySelector('[data-summary="who-win"]').style.color = "red";
+    } else {
+        document.querySelector('p.draws span').textContent = ++gameSummary.draws;
+        document.querySelector('[data-summary="who-win"]').textContent = "Nobody won...";
+        document.querySelector('[data-summary="who-win"]').style.color = "blue";
+    }
+}
+
+function endGame() {
+    document.querySelector(`[data-option = "${game.playerHand}"]`).style.boxShadow = "";
+    game.playerHand = "";
+    game.aiHand = "";
+}
+
+//main func
 function startGame() {
     if (!game.playerHand) return alert("Select hand!");
     // console.log(aiChoice());
     game.aiHand = aiChoice();
     const gameResult = checkResult(game.playerHand, game.aiHand);
     console.log(gameResult);
-
+    publishResult(game.playerHand, game.aiHand, gameResult);
+    endGame();
 }
 
 hands.forEach(hand => hand.addEventListener("click", handSelection));
@@ -53,4 +78,4 @@ hands.forEach(hand => hand.addEventListener("click", handSelection));
 document.querySelector(".start").addEventListener("click", startGame);
 
 
-//done with udemy - webSamuraj
+//done as part of udemy course "Programowanie w JavaScript"
